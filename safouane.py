8265f491e3cb5749b93253b7dc5987b9a9f8ea9b
Ilyas <ilyassen@users.noreply.github.com>
2023-09-13 15:41:01 -0400

    loop break

--- safouane.py
+++ safouane.py
@@ -1,3 +1,21 @@
-text = 4 * 'un' + 'ium'
-test = ''
-print(text)
+n = 2
+i = 1
+
+for n in range(1, 5):
+    for i in range(1,5):
+        if i == n :
+            break
+        print(i)
+
+
+# class Dog:                   
+ 
+#     def __init__(self,dogBreed="nasus",dogEyeColor="black"):
+    
+#         self.breed = dogBreed       
+#         self.eyeColor = dogEyeColor
+    
+	
+# dog1 = Dog()
+
+# print(dog1.__getattribute__("breed"))
