73d4300d6d4638c1da48e59000d386560492cb59
Ilyas <ilyassen@users.noreply.github.com>
2023-09-13 16:03:24 -0400

    Add examples of class dog

--- safouane.py
+++ safouane.py
@@ -1,21 +1,30 @@
-n = 2
-i = 1
 
-for n in range(1, 5):
-    for i in range(1,5):
-        if i == n :
-            break
-        print(i)
-
-
-# class Dog:                   
+class Dog:                   
  
-#     def __init__(self,dogBreed="nasus",dogEyeColor="black"):
+    def __init__(self,dogBreed,dogEyeColor):
     
-#         self.breed = dogBreed       
-#         self.eyeColor = dogEyeColor
-    
-	
-# dog1 = Dog()
+        self.breed = dogBreed       
+        self.eyeColor = dogEyeColor
 
-# print(dog1.__getattribute__("breed"))
+    def get_breed(self):
+        return self.breed
+
+    def get_eyeColor(self):
+        return self.eyeColor
+    
+    def display(self):
+        print("Breed : " + self.get_breed())
+        print("Eye Color : " +self.get_eyeColor())
+
+dog1 = Dog("Nasus","Black")
+dog2 = Dog("ChienLoup","Blue")
+
+Dogs_list = []
+Dogs_list.append(dog1)
+
+Dogs_list.append(dog2)
+
+for item in Dogs_list:
+    print(" ----- Item ------")
+    item.display()
+    print(" -----------------")
