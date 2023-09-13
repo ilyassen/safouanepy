55b2c4c01af36941308dc2d201865eec97408114
Conteriano <safafize@gmail.com>
2023-09-13 22:26:03 +0200

    truth

--- safouane.py
+++ safouane.py
@@ -1,30 +1,54 @@
 
-class Dog:                   
+class Champion:                   
  
-    def __init__(self,dogBreed,dogEyeColor):
-    
-        self.breed = dogBreed       
-        self.eyeColor = dogEyeColor
+    def __init__(self,name,championsexe,place,year,role,power):
+        self.name = name
+        self.gender = championsexe     
+        self.place = place
+        self.year = year
+        self.role = role 
+        self.power = power
 
-    def get_breed(self):
-        return self.breed
+    def get_gender(self):
+        return self.gender
 
-    def get_eyeColor(self):
-        return self.eyeColor
+    def get_place(self):
+        return self.place
     
+    def get_year(self):
+        return self.year
+    
+    def get_role(self):
+        return self.role
+    
+    def get_power(self):
+        return self.power
+    def get_name(self):
+        return self.name
+
     def display(self):
-        print("Breed : " + self.get_breed())
-        print("Eye Color : " +self.get_eyeColor())
+        print("Name: " + self.get_name())
+        print("Gender : " + self.get_gender())
+        print("place : " + self.get_place())
+        print("year : " + self.get_year())
+        print("role : " + self.get_role())
+        print("power : " +self.get_power())
 
-dog1 = Dog("Nasus","Black")
-dog2 = Dog("ChienLoup","Blue")
+champion1 = Champion("Zilean","Male","Shhurima","2009","mid/support","VERY OP")
+champion2 = Champion("Warwick","who knows","Zhaun","2009","Top/jungle","VERY tilti")
 
-Dogs_list = []
-Dogs_list.append(dog1)
 
-Dogs_list.append(dog2)
+champion_list = []
+champion_list.append(champion1)
 
-for item in Dogs_list:
-    print(" ----- Item ------")
-    item.display()
-    print(" -----------------")
+champion_list.append(champion2)
+
+print("CHKON A7SAN CHAMPION F L3ALAM")
+print(" ----- CHAMPION LI WA3ER BEZAAAF------")
+champion1.display()
+print(" -----------------")
+
+print("CHKON LI M9TOUL MARRA W MSALI")
+print(" ----- YOU ASKED FOR IT------ ")
+champion2.display()
+print(" -----------------")
